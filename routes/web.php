cc849c49da24e050ce86687320515c25a967b44a
Rodrigo Silva <2242086@my.ipleiria.pt>
2025-04-14 16:56:36 +0100

    Add all files

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () { //Redireciona para a dashboard
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard'); //Só users verificados é que são encaminhados
+
+Route::middleware('auth')->group(function () { //Operações CRUD
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
